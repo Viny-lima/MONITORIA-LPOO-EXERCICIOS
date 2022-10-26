dec994c7b37acdb5de7bf86b6b67fe8c6c77b4df
Vinicius Lima <82112071+Viny-lima@users.noreply.github.com>
2022-10-26 19:43:07 -0300

    Resolved Question 2

--- src/MEF09/Program.java
+++ src/MEF09/Program.java
@@ -1,7 +1,5 @@
 package MEF09;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Program {
@@ -10,7 +8,7 @@ public class Program {
 	{
 		while(true) 
 		{			
-			Q1();
+			Q2();
 		}
 
 	}
@@ -18,7 +16,7 @@ public class Program {
 	private static String Input() 
 	{		
 		Scanner scan = new Scanner(System.in);
-		return scan.next();		
+		return scan.nextLine();
 	}
 	
 	private static void Print(String string) 
@@ -33,22 +31,21 @@ public class Program {
 		{
 			try 
 			{			
-				Scanner scanner = new Scanner(System.in);
-				
-				System.out.println("");
-				System.out.println("========================");
-				
-				System.out.println("DIGITE O NOME: ");
-				String name = scanner.next();
-				
-				System.out.println("DIGITE O NÚMERO: ");
-				String numString = scanner.next();
-				Integer num = Integer.parseInt(numString);
-				
-				char letter = name.toCharArray()[num];
-				
-				System.out.println("");
-				Print("Resultado:" + letter);						
+					Scanner scanner = new Scanner(System.in);
+					System.out.println("");
+					System.out.println("========================");
+					
+					System.out.println("DIGITE O NOME: ");
+					String name = scanner.nextLine();
+					
+					System.out.println("DIGITE O NÚMERO: ");
+					String numString = scanner.next();
+					Integer num = Integer.parseInt(numString);
+					
+					char letter = name.toCharArray()[num];
+					
+					System.out.println("");
+					Print("Resultado:" + letter);								
 			}
 			catch (ArrayIndexOutOfBoundsException e) 
 			{			
@@ -64,22 +61,34 @@ public class Program {
 
 	public static void Q2() 
 	{
-		String[] nomes = new String[10]; 
- 		
-		for (int i = 0; i <= 10; i++) 
+		try 
+		{				
+			ArrayList<Integer> olds = new ArrayList<Integer>(); 
+	 		
+			for (int i = 0; i <= 10; i++) 
+			{
+				Print("DIGITE O IDADE DA PESSOA [" + i + "]" );
+				var numString = Input();
+				var n = Integer.parseInt(numString);
+				olds.add(n) ;			
+			}	
+			
+			Print(olds.toString());				
+			Print("DIGITE O NÚMERO");
+			var numString = Input();
+			var num = Integer.parseInt(numString);
+			olds.remove(num);		
+			Print(olds.toString());					
+		}
+		catch (IndexOutOfBoundsException e)
+		{			
+			Print("Posição Inválida");
+		}
+		catch (NumberFormatException e) 
 		{
-			Print("DIGITE O IDADE DA PESSOA [" + i + "]" );
-			nomes[i] = Input();			
-		}		
+			Print("Por favor digite somente números inteiros !");
+		}	
 		
-		Print("DIGITE O NÚMERO");
-		var numString = Input();
-		var num = Integer.parseInt(numString);	
-		
-		nomes[num] = "";		
-		
-		
-		
-		
+		Print("====================================");
 	}
 }
